6eae1e62575c0cdfeb06bc82d681bef82e57ac25
Akshat Sethi <masterakshata@gmail.com>
2022-01-07 14:22:37 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -6,24 +6,6 @@ const App = () => {
   const [width, setWidth] = useState(320)
   return (
     <div id="main">
-      <form>
-        <label htmlFor='height-slider'>Height Slider</label>
-        <input
-          type={"range"}
-          min="100" max="800"
-          value={height}
-          onChange={(e) => setHeight(e.target.value)}
-          id="height-slider" />
-        <br />
-        <label htmlFor='width-slider'>Width Slider</label>
-        <input
-          type={"range"}
-          min="100" max="800"
-          value={width}
-          onChange={(e) => setWidth(e.target.value)}
-          id="width-slider" />
-      </form>
-      <img src={imgSrc} style={{ height: `${height}px`, width: `${width}px` }} id="resizable-img" />
 
     </div>
   )
