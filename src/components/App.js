c25eb44ef878475566f9a0175813d98a02626cf0
Akshat Sethi <masterakshata@gmail.com>
2022-01-07 14:27:32 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../styles/App.css';
-import imgSrc from '../image.jpeg'
+
 const App = () => {
-  const [height, setHeight] = useState(320)
-  const [width, setWidth] = useState(320)
   return (
     <div id="main">
 
